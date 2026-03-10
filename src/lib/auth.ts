4764de5ab3dc8e2919f42c5866de4b10e42677fc
Thomas Soares <thomas__soares@hotmail.com>
2026-03-10 11:18:49 -0300

    feat: Setup Prisma and Docker.

---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
+
+import { prisma } from "./db.ts";
+import { env } from "./env.ts";
+
+export const auth = betterAuth({
+  baseURL: env.API_BASE_URL,
+  trustedOrigins: [env.WEB_APP_BASE_URL],
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [openAPI()],
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true,
+      domain:
+        env.NODE_ENV === "production" ? ".fullstackclub.com.br" : undefined,
+    },
+  },
+});
---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../generated/prisma/client.js";
+import { env } from "./env.ts";
+
+const connectionString = `${env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
